056c39e98dbfcfbde3cc9d25ac8c0a065c4c00cd
Chun <c33wang@gmail.com>
2015-08-03 13:20:28 -0700

    first

---
+++ OneInterop.py
@@ -0,0 +1,26 @@
+import os, warnings
+os.chdir('/Users/ubnt/githubrepo/ixia9/')
+
+from ixia.webapi import *
+
+###                            suppress warnings                                 ###
+warnings.filterwarnings("ignore")
+
+###                                 login credintials                            ###
+webServerAddress = "https://10.1.0.199"
+version = "v1"
+user = "chun"
+password = "ubnt"
+
+###connects to server
+api = webApi.connect(webServerAddress, version, None, user, password)
+###joins session #26
+session = api.joinSession(26)
+###run sessions
+
+print "Test is running ......"
+result = session.runTest()
+
+save_path = "/Users/ubnt/Documents/testResults.zip"
+with open(save_path, "a+") as statsFile:
+	api.getStatsCsvZipToFile(result.testId, statsFile)
